840230b8c87b5671537afeee247687c79756a684
nicatino <epicjew.nougoi@gmail.com>
2022-09-03 13:39:52 +0300

    Solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,75 @@
 'use strict';
 
-describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+describe('ifElse function', () => {
+  const trueCondition = () => true;
+  const falseCondition = () => false;
+  const mockCBTrue = jest.fn();
+  const mockCBFalse = jest.fn();
 
+  it('should be a function', () => {
+    expect(ifElse)
+      .toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it('should return undefined', () => {
+    expect(ifElse(trueCondition, mockCBTrue, mockCBFalse))
+      .toBe(undefined);
+  });
+  
+  it('should call first function if initial is true', () => {
+      ifElse(trueCondition, mockCBTrue, mockCBFalse);
+      expect(mockCBTrue).toHaveBeenCalled();
+      expect(mockCBFalse).not.toHaveBeenCalled();
+  });
+  
+  it('should call second function if initial is false', () => {
+    ifElse(falseCondition, mockCBTrue, mockCBFalse);
+      expect(mockCBFalse).toHaveBeenCalled();
+      expect(mockCBTrue).not.toHaveBeenCalled();
+  });
+
+  it(`should call second function if initial is undefined`, () => {
+    ifElse(() => undefined, mockCBTrue, mockCBFalse);
+
+    expect(mockCBTrue).not.toHaveBeenCalled();
+    expect(mockCBFalse).toHaveBeenCalled();
+  });
+          
+  it(`should call second function if initial is numeric`, () => {
+    ifElse(() => 1, mockCBTrue, mockCBFalse);
+
+    expect(mockCBTrue).not.toHaveBeenCalled();
+    expect(mockCBFalse).toHaveBeenCalled();
+  });
+            
+  it(`should call second function if initial is string`, () => {
+    ifElse(() => 'true', mockCBTrue, mockCBFalse);
+
+    expect(mockCBTrue).not.toHaveBeenCalled();
+    expect(mockCBFalse).toHaveBeenCalled();
+  });
+  
+  it(`should call first function several times if initial is true` +
+      `and called more than once`, () => {
+    ifElse(trueCondition, mockCBTrue, mockCBFalse);
+    ifElse(trueCondition, mockCBTrue, mockCBFalse);
+    ifElse(trueCondition, mockCBTrue, mockCBFalse);
+
+    expect(mockCBTrue).toHaveBeenCalled();
+    expect(mockCBTrue.mock.calls.length).toBe(3);
+    expect(mockCBFalse).not.toHaveBeenCalled();
+  });
+    
+  it(`should call first function several times if initial is false` +
+      `and called more than once`, () => {
+    ifElse(falseCondition, mockCBTrue, mockCBFalse);
+    ifElse(falseCondition, mockCBTrue, mockCBFalse);
+    ifElse(falseCondition, mockCBTrue, mockCBFalse);
+
+    expect(mockCBTrue).not.toHaveBeenCalled();
+    expect(mockCBFalse).toHaveBeenCalled();
+    expect(mockCBFalse.mock.calls.length).toBe(3);
+  });
 });
