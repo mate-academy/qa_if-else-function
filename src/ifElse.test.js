7882257361b509d00e596af136df2a5fae810db7
sergii-nosachenko <s.nosachenko@gmail.com>
2022-10-04 11:29:30 +0300

    Added tests

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,52 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
-
+  it('is declared', () => {
+    expect(ifElse)
+      .toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it('returns nothing', () => {
+    expect(ifElse(() => {}, () => {}, () => {}))
+      .toBeUndefined();
+  });
+
+  it('calls condition fn', () => {
+    const condition = jest.fn();
+
+    ifElse(condition, () => {}, () => {});
+
+    expect(condition)
+      .toBeCalled();
+  });
+
+  it('calls first fn if condition() === true', () => {
+    const condition = jest.fn().mockReturnValue(true);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(first)
+      .toBeCalled();
+
+    expect(second)
+      .not.toBeCalled();
+  });
+
+  it('calls second fn if condition() !== true', () => {
+    const condition = jest.fn().mockReturnValue(false);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(first)
+      .not.toBeCalled();
+
+    expect(second)
+      .toBeCalled();
+  });
 });
