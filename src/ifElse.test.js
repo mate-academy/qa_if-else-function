cd4a187d246580957240aea3f51ee6b9d4dc8016
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2022-11-06 23:54:47 +0200

    Tests

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,55 @@
 'use strict';
 
-describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+describe(`'ifElse' function`, () => {
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+  let firstCallback;
+  let secondCallback;
 
+  beforeEach(() => {
+    firstCallback = jest.fn();
+    secondCallback = jest.fn();
   });
 
-  // write tests here
+  it('should be declared', function() {
+    expect(ifElse)
+      .toBeInstanceOf(Function);
+  });
+
+  it('should return undefined', function() {
+    const condition = () => false;
+
+    expect(ifElse(condition, firstCallback, secondCallback))
+      .toBeUndefined();
+  });
+
+  it('should run the first callback if te condition return true', function() {
+    const condition = jest.fn(() => true);
+
+    ifElse(condition, firstCallback, secondCallback);
+
+    expect(condition)
+      .toHaveBeenCalled();
+
+    expect(firstCallback)
+      .toHaveBeenCalled();
+
+    expect(secondCallback)
+      .not.toHaveBeenCalled();
+  });
+
+  it('should run the second callback if te condition return false', function() {
+    const condition = jest.fn(() => false);
+
+    ifElse(condition, firstCallback, secondCallback);
+
+    expect(condition)
+      .toHaveBeenCalled();
+
+    expect(firstCallback)
+      .not.toHaveBeenCalled();
+
+    expect(secondCallback)
+      .toHaveBeenCalled();
+  });
 });
