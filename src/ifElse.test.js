a66ffbdaf572f5224da9a9c87bd1fba3759e3348
Andrii Dydyk <andriidydyk622@gmail.com>
2023-02-19 12:15:59 +0100

    Solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,54 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
+  let testA, testB;
 
-  it('should ', () => {
+  beforeEach(() => {
+    testA = 0;
+    testB = 0;
+  });
 
+  it('should be declared', () => {
+    expect(ifElse).toBeInstanceOf(Function);
   });
 
   // write tests here
+  it(`shouldn't return an anything`, () => {
+    const result = ifElse(
+      () => true,
+      () => {},
+      () => {}
+    );
+
+    expect(result).toBeUndefined();
+  });
+
+  it(`should call 'first' function if 'condition' returns true`, () => {
+    ifElse(
+      () => true,
+      () => {
+        testA = 1;
+      },
+      () => {
+        testB = 1;
+      }
+    );
+    expect(testA).toEqual(1);
+    expect(testB).toEqual(0);
+  });
+
+  it(`should call 'second' function if 'condition' returns true`, () => {
+    ifElse(
+      () => false,
+      () => {
+        testA = 1;
+      },
+      () => {
+        testB = 1;
+      }
+    );
+    expect(testA).toEqual(0);
+    expect(testB).toEqual(1);
+  });
 });
