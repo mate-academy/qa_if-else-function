5f87ba33d4ce1fe2c295e274052652364821602e
OlhaArama <arama_o_f@ukr.net>
2023-04-28 15:04:29 +0300

    Solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,53 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
-
+  it(`should be declared`, () => {
+    expect(ifElse).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it('should run a first callback if condition returns true', () => {
+    const condition = jest.fn(() => true);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(first)
+      .toHaveBeenCalled();
+  });
+
+  it('should not run a second callback if condition returns true', () => {
+    const condition = jest.fn(() => true);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(second)
+      .not.toHaveBeenCalled();
+  });
+
+  it('should run a second callback if condition returns false', () => {
+    const condition = jest.fn(() => false);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(second)
+      .toHaveBeenCalled();
+  });
+
+  it('should not run a first callback if condition returns false', () => {
+    const condition = jest.fn(() => false);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(first)
+      .not.toHaveBeenCalled();
+  });
 });
