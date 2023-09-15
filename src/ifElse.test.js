080c3240e9a6c49d457ee29fe45594466ef094a1
Bohdan Svystun <bogdansvystun02@gmail.com>
2023-09-15 14:48:03 +0300

    Solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,57 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+  it('should call the second callback when condition returns false', () => {
+    let conditionCalled = false;
+    let firstCallbackCalled = false;
+    let secondCallbackCalled = false;
 
+    const condition = () => {
+      conditionCalled = true;
+
+      return false;
+    };
+
+    const firstCallback = () => {
+      firstCallbackCalled = true;
+    };
+
+    const secondCallback = () => {
+      secondCallbackCalled = true;
+    };
+
+    ifElse(condition, firstCallback, secondCallback);
+
+    expect(conditionCalled).toBeTruthy();
+    expect(firstCallbackCalled).toBeFalsy();
+    expect(secondCallbackCalled).toBeTruthy();
   });
 
-  // write tests here
+  it('should call the first callback when condition returns true', () => {
+    let conditionCalled = false;
+    let firstCallbackCalled = false;
+    let secondCallbackCalled = false;
+
+    const condition = () => {
+      conditionCalled = true;
+
+      return true;
+    };
+
+    const firstCallback = () => {
+      firstCallbackCalled = true;
+    };
+
+    const secondCallback = () => {
+      secondCallbackCalled = true;
+    };
+
+    ifElse(condition, firstCallback, secondCallback);
+
+    expect(conditionCalled).toBeTruthy();
+    expect(firstCallbackCalled).toBeTruthy();
+    expect(secondCallbackCalled).toBeFalsy();
+  });
 });
