c7afbce56fa950bf1e8bd9b9e42a53991e645b9e
Olesia Semen <semenolesa01@gmail.com>
2023-10-18 19:21:43 +0300

    Testing

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,25 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+ const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+ it('should run the first callback if condition returns true', () => {
+  const condition = jest.fn(() => true);
+  const firstCallback = jest.fn();
 
-  });
+  ifElse(condition, firstCallback, () => {});
 
-  // write tests here
+  expect(condition).toHaveBeenCalled();
+  expect(firstCallback).toHaveBeenCalled();
+});
+
+it('should run the second callback if condition returns false', () => {
+  const condition = jest.fn(() => false);
+  const secondCallback = jest.fn();
+
+  ifElse(condition, () => {}, secondCallback);
+
+  expect(condition).toHaveBeenCalled();
+  expect(secondCallback).toHaveBeenCalled();
+});
 });
