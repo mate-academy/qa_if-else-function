2d89c6bf598f0570543d9b1b670eaa414e7ba566
Oleksii Bieliaiev <o.bieliaiev@icloud.com>
2023-11-15 14:20:43 +0200

    solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,37 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+  it('should run the first callback if condition returns true', () => {
+    let result = 0;
 
+    ifElse(
+      () => true,
+      () => {
+        result = 1;
+      },
+      () => {
+        result = 2;
+      }
+    );
+
+    expect(result).toBe(1);
   });
 
-  // write tests here
+  it('should run the second callback if condition returns false', () => {
+    let result = 0;
+
+    ifElse(
+      () => false,
+      () => {
+        result = 1;
+      },
+      () => {
+        result = 2;
+      }
+    );
+
+    expect(result).toBe(2);
+  });
 });
