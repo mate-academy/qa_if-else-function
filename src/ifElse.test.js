3152ac98dcc920bc97962c9dd9650e7cb9c8bf44
sheva-laptop <vladislavshevchenko10@gmail.com>
2023-12-13 20:47:39 +0200

    add tests

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,37 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+  const conditionTrueMock = jest.fn(() => true);
+  const conditionFalseMock = jest.fn(() => false);
 
+  let firstCallbackMock;
+  let secondCallbackMock;
+
+  beforeEach(() => {
+    firstCallbackMock = jest.fn();
+    secondCallbackMock = jest.fn();
   });
 
-  // write tests here
+  afterEach(() => {
+    firstCallbackMock.mockReset();
+    secondCallbackMock.mockReset();
+  });
+
+  it('should call the first callback if condition returns true', () => {
+    ifElse(conditionTrueMock, firstCallbackMock, secondCallbackMock);
+
+    expect(conditionTrueMock).toHaveBeenCalled();
+    expect(firstCallbackMock).toHaveBeenCalled();
+    expect(secondCallbackMock).not.toHaveBeenCalled();
+  });
+
+  it('should call the second callback if condition returns false', () => {
+    ifElse(conditionFalseMock, firstCallbackMock, secondCallbackMock);
+
+    expect(conditionFalseMock).toHaveBeenCalled();
+    expect(firstCallbackMock).not.toHaveBeenCalled();
+    expect(secondCallbackMock).toHaveBeenCalled();
+  });
 });
