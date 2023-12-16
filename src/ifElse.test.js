d4a7e9e4b611cd2914135cdc1eff467bf1fd3818
Aleksandr <cebanoleksandr@gmail.com>
2023-12-16 10:02:04 +0200

    solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,58 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
+  let firstFunction;
+  let secondFunction;
 
-  it('should ', () => {
-
+  beforeEach(() => {
+    firstFunction = jest.fn();
+    secondFunction = jest.fn();
   });
 
-  // write tests here
+  afterEach(() => {
+    firstFunction = null;
+    secondFunction = null;
+  });
+
+  it(`should be declared`, () => {
+    expect(ifElse)
+      .toBeInstanceOf(Function);
+  });
+
+  it(`should call the first function if condition is true`, () => {
+    const condition = jest.fn(() => true);
+
+    ifElse(condition, firstFunction, secondFunction);
+
+    expect(firstFunction)
+      .toHaveBeenCalled();
+  });
+
+  it(`should not call the second function if condition is true`, () => {
+    const condition = jest.fn(() => true);
+
+    ifElse(condition, firstFunction, secondFunction);
+
+    expect(secondFunction)
+      .not.toHaveBeenCalled();
+  });
+
+  it(`should not call the first function if condition is false`, () => {
+    const condition = jest.fn(() => false);
+
+    ifElse(condition, firstFunction, secondFunction);
+
+    expect(firstFunction)
+      .not.toHaveBeenCalled();
+  });
+
+  it(`should call the second function if condition is false`, () => {
+    const condition = jest.fn(() => false);
+
+    ifElse(condition, firstFunction, secondFunction);
+
+    expect(secondFunction)
+      .toHaveBeenCalled();
+  });
 });
