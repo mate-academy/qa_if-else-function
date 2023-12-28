2812d1b1ab61a6857a44ad30ae0916a57be72378
Kate <ekaterina2002aleks@gmail.com>
2023-12-28 20:09:25 +0200

    Solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,33 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+  it('should call first cb 1 time if condition = true', () => {
+    const f1 = jest.fn();
+    const f2 = jest.fn();
 
+    ifElse(
+      () => true,
+      f1,
+      f2,
+    );
+
+    expect(f1).toHaveBeenCalledTimes(1);
+    expect(f2).toHaveBeenCalledTimes(0);
   });
 
-  // write tests here
+  it('should call second cb 1 time if condition = false', () => {
+    const f1 = jest.fn();
+    const f2 = jest.fn();
+
+    ifElse(
+      () => false,
+      f1,
+      f2,
+    );
+
+    expect(f1).toHaveBeenCalledTimes(0);
+    expect(f2).toHaveBeenCalledTimes(1);
+  });
 });
