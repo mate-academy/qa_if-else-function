9438e0bee3781243324639a7c989f4dc1dae13d1
ivanm2706 <ivanmorhun664@gmail.com>
2024-03-22 17:28:14 -0500

    solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,64 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
+  let firstCallback;
+  let secondCallback;
+  let condition;
 
-  it('should ', () => {
-
+  beforeEach(() => {
+    firstCallback = jest.fn();
+    secondCallback = jest.fn();
+    condition = jest.fn();
   });
 
-  // write tests here
+  afterEach(() => {
+    firstCallback.mockClear();
+    secondCallback.mockClear();
+    condition.mockClear();
+  });
+
+  it('should call condition 1 time', () => {
+    ifElse(condition, firstCallback, secondCallback);
+
+    expect(condition).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call firstCallback if comdition return - true', () => {
+    condition = condition
+      .mockReturnValueOnce(true);
+
+    ifElse(condition, firstCallback, secondCallback);
+
+    expect(firstCallback).toHaveBeenCalled();
+    expect(secondCallback).not.toHaveBeenCalled();
+  });
+
+  it('should call firstCallback once if comdition return - true', () => {
+    condition = condition
+      .mockReturnValueOnce(true);
+
+    ifElse(condition, firstCallback, secondCallback);
+
+    expect(firstCallback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call secondCallback if comdition return - false', () => {
+    condition = condition
+      .mockReturnValueOnce(false);
+
+    ifElse(condition, firstCallback, secondCallback);
+
+    expect(firstCallback).not.toHaveBeenCalled();
+    expect(secondCallback).toHaveBeenCalled();
+  });
+
+  it('should call secondCallback once if comdition return - false', () => {
+    condition = condition
+      .mockReturnValueOnce(false);
+
+    ifElse(condition, firstCallback, secondCallback);
+
+    expect(secondCallback).toHaveBeenCalledTimes(1);
+  });
 });
