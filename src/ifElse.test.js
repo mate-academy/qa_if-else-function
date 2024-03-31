3d44ea863d443e657b7efa0c0d78d87051adfeb0
bla2001 <blasmartlab@gmail.com>
2024-03-31 20:35:58 +0300

    ifElse tests

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,41 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+  let firstCallback;
+  let secondCallback;
 
+  beforeEach(() => {
+    firstCallback = jest.fn();
+    secondCallback = jest.fn();
   });
 
-  // write tests here
+  it('should be defined', () => {
+    expect(ifElse).toBeDefined();
+  });
+
+  it('should run first callback if condition returns true', () => {
+    const condition = jest.fn(() => true);
+
+    ifElse(condition, firstCallback, secondCallback);
+    expect(condition).toHaveBeenCalled();
+    expect(firstCallback).toHaveBeenCalled();
+    expect(secondCallback).not.toHaveBeenCalled();
+  });
+
+  it('should run second callback if condition returns false', () => {
+    const condition = jest.fn(() => false);
+
+    ifElse(condition, firstCallback, secondCallback);
+    expect(condition).toHaveBeenCalled();
+    expect(firstCallback).not.toHaveBeenCalled();
+    expect(secondCallback).toHaveBeenCalled();
+  });
+
+  it('should return undefined', () => {
+    const result = ifElse(() => {}, () => {}, () => {});
+
+    expect(result).toBeUndefined();
+  });
 });
