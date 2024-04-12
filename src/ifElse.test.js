70f3fcab229584d2d4f4bc93c677b9adb22414e3
Bogdan Stratiychuk <stratiychuk.bogdan@gmail.com>
2024-04-12 16:20:51 +0200

    Solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,30 @@
 'use strict';
 
+const { ifElse } = require("./ifElse");
+
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+  it('condition = true -> run first callback', () => {
+    const condition = ()=> true;
 
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second)
+
+    expect(first).toHaveBeenCalled();
+    expect(second).not.toHaveBeenCalled();
   });
 
-  // write tests here
+  it('condition = false -> run second callback', () => {
+    const condition = ()=> false;
+
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second)
+
+    expect(first).not.toHaveBeenCalled();
+    expect(second).toHaveBeenCalled();
+  });
 });
