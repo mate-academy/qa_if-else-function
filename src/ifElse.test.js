5309662494d1a79149fda18b799196c9ec6a0e1b
Oleksandr Sherashov <apbreloaded947@gmail.com>
2024-04-12 13:31:55 +0300

    add tests

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,34 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
+  let mockFirst;
+  let mockSecond;
 
-  it('should ', () => {
-
+  beforeEach(() => {
+    mockFirst = jest.fn();
+    mockSecond = jest.fn();
   });
 
-  // write tests here
+  it(`should be a function`, () => {
+    expect(ifElse).toBeInstanceOf(Function);
+  });
+
+  it(`should call first callback if 'condition()' === true`, () => {
+    const mockFuncCondition = jest.fn(() => true);
+
+    ifElse(mockFuncCondition, mockFirst, mockSecond);
+
+    expect(mockFirst.mock.calls.length).toBe(1);
+    expect(mockSecond.mock.calls.length).toBe(0);
+  });
+
+  it(`should call second callback if 'condition()' === false`, () => {
+    const mockFuncCondition = jest.fn(() => false);
+
+    ifElse(mockFuncCondition, mockFirst, mockSecond);
+
+    expect(mockFirst.mock.calls.length).toBe(0);
+    expect(mockSecond.mock.calls.length).toBe(1);
+  });
 });
