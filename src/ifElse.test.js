df533f2d7d7fd9fa535ff6e3df902e7e88ae2be3
Dmytro <dakorozer@gmail.com>
2024-05-02 13:35:28 +0300

    solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,31 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
-
+  it('should be declared', () => {
+    expect(ifElse).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it(`should return 'first' if 'condition' is true`, () => {
+    const condition = () => true;
+    const firstCb = jest.fn();
+    const secondCb = jest.fn();
+
+    ifElse(condition, firstCb, secondCb);
+
+    expect(firstCb).toHaveBeenCalled();
+    expect(secondCb).not.toHaveBeenCalled();
+  });
+  
+  it(`should return 'second' if 'condition' is false`, () => {
+    const condition = () => false;
+    const firstCb = jest.fn();
+    const secondCb = jest.fn();
+
+    ifElse(condition, firstCb, secondCb);
+
+    expect(firstCb).not.toHaveBeenCalled();
+    expect(secondCb).toHaveBeenCalled();
+  });
 });
