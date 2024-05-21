50a7b63049231d13a823c957ee337e05081e2ffa
Roksolana Kalytyn <roksa.kalytyn@gmail.com>
2024-05-21 14:58:38 +0300

    solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,45 @@
 'use strict';
 
+const { ifElse } = require('./ifElse');
+
 describe('ifElse', () => {
   // const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+  let conditionMock;
+  let firstMock;
+  let secondMock;
 
+  beforeEach(() => {
+    conditionMock = jest.fn();
+    firstMock = jest.fn();
+    secondMock = jest.fn();
   });
 
-  // write tests here
+  it('should call first callback when condition return true', () => {
+    conditionMock.mockReturnValue(true);
+
+    ifElse(conditionMock, firstMock, secondMock);
+
+    expect(conditionMock).toHaveBeenCalled();
+    expect(firstMock).toHaveBeenCalled();
+    expect(secondMock).not.toHaveBeenCalled();
+  });
+
+  it('should call second callback when condition return false', () => {
+    conditionMock.mockReturnValue(false);
+
+    ifElse(conditionMock, firstMock, secondMock);
+
+    expect(conditionMock).toHaveBeenCalled();
+    expect(firstMock).not.toHaveBeenCalled();
+    expect(secondMock).toHaveBeenCalled();
+  });
+
+  it('should not return any value', () => {
+    conditionMock.mockReturnValue(true);
+
+    const result = ifElse(conditionMock, firstMock, secondMock);
+
+    expect(result).toBe(undefined);
+  });
 });
