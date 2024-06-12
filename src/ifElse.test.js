f992448c7857e8cf94f2d484be992f5e24cd6614
Oleksandr Bielov <alehandro.bel96@gmail.com>
2024-06-12 10:25:48 +0300

    Solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,88 @@
 'use strict';
 
-describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+describe(`'ifElse' function:`, () => {
+  it(`should call a 'first' callback `
+      + `if 'condition' returns boolean 'true'`, () => {
+    const condition = jest.fn(() => true);
+    const firstCb = jest.fn(() => {});
+    const secondCb = jest.fn(() => {});
 
+    ifElse(condition, firstCb, secondCb);
+
+    expect(firstCb).toHaveBeenCalled();
   });
 
-  // write tests here
+  it(`should return nothing`, () => {
+    const condition = jest.fn(() => true);
+    const firstCb = jest.fn(() => {});
+    const secondCb = jest.fn(() => {});
+
+    const isElse = ifElse(condition, firstCb, secondCb);
+
+    expect(isElse).toBeUndefined();
+  });
+
+  describe(`should call a 'second' callback if 'condition' is falsy:`, () => {
+    it(`if 'condition' callback returns 'false'`, () => {
+      const condition = jest.fn(() => false);
+      const firstCb = jest.fn(() => {});
+      const secondCb = jest.fn(() => {});
+
+      ifElse(condition, firstCb, secondCb);
+
+      expect(secondCb).toHaveBeenCalled();
+    });
+
+    it(`if 'condition' callback returns 0`, () => {
+      const condition = jest.fn(() => 0);
+      const firstCb = jest.fn(() => {});
+      const secondCb = jest.fn(() => {});
+
+      ifElse(condition, firstCb, secondCb);
+
+      expect(secondCb).toHaveBeenCalled();
+    });
+
+    it(`if 'condition' callback returns an empty string`, () => {
+      const condition = jest.fn(() => '');
+      const firstCb = jest.fn(() => {});
+      const secondCb = jest.fn(() => {});
+
+      ifElse(condition, firstCb, secondCb);
+
+      expect(secondCb).toHaveBeenCalled();
+    });
+
+    it(`if 'condition' callback returns 'null'`, () => {
+      const condition = jest.fn(() => null);
+      const firstCb = jest.fn(() => {});
+      const secondCb = jest.fn(() => {});
+
+      ifElse(condition, firstCb, secondCb);
+
+      expect(secondCb).toHaveBeenCalled();
+    });
+
+    it(`if 'condition' callback returns 'undefined'`, () => {
+      const condition = jest.fn(() => undefined);
+      const firstCb = jest.fn(() => {});
+      const secondCb = jest.fn(() => {});
+
+      ifElse(condition, firstCb, secondCb);
+
+      expect(secondCb).toHaveBeenCalled();
+    });
+
+    it(`if 'condition' callback returns 'NaN'`, () => {
+      const condition = jest.fn(() => NaN);
+      const firstCb = jest.fn(() => {});
+      const secondCb = jest.fn(() => {});
+
+      ifElse(condition, firstCb, secondCb);
+
+      expect(secondCb).toHaveBeenCalled();
+    });
+  });
 });
