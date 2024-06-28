378f5e0ed742b738a1abfc4dbcbb10573017bfe5
Ilya <pulcho91@gmail.com>
2024-06-28 11:21:49 +0300

    Solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -3,70 +3,66 @@
 describe('ifElse', () => {
   const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
-
-  });
-
   it('should call the first function when the condition is true', () => {
     const condition = jest.fn().mockReturnValue(true);
-    const first = jest.fn();
-    const second = jest.fn();
+    const firstCallback = jest.fn();
+    const secondCallback = jest.fn();
 
-    ifElse(condition, first, second);
+    ifElse(condition, firstCallback, secondCallback);
 
-    expect(first).toHaveBeenCalled();
-    expect(second).not.toHaveBeenCalled();
+    expect(firstCallback).toHaveBeenCalled();
+    expect(secondCallback).not.toHaveBeenCalled();
   });
 
   it('should call the second function when the condition is false', () => {
     const condition = jest.fn().mockReturnValue(false);
-    const first = jest.fn();
-    const second = jest.fn();
+    const firstCallback = jest.fn();
+    const secondCallback = jest.fn();
 
-    ifElse(condition, first, second);
+    ifElse(condition, firstCallback, secondCallback);
 
-    expect(first).not.toHaveBeenCalled();
-    expect(second).toHaveBeenCalled();
+    expect(firstCallback).not.toHaveBeenCalled();
+    expect(secondCallback).toHaveBeenCalled();
   });
 
   it('call first function when the condition returns true(real value)', () => {
     const condition = () => Math.random() > 0.5;
-    const first = jest.fn();
-    const second = jest.fn();
+    const firstCallback = jest.fn();
+    const secondCallback = jest.fn();
 
     Math.random = jest.fn().mockReturnValue(0.6);
 
-    ifElse(condition, first, second);
+    ifElse(condition, firstCallback, secondCallback);
 
-    expect(first).toHaveBeenCalled();
-    expect(second).not.toHaveBeenCalled();
+    expect(firstCallback).toHaveBeenCalled();
+    expect(secondCallback).not.toHaveBeenCalled();
   });
 
   it('call second function when condition returns false (real value)', () => {
     const condition = () => Math.random() > 0.5;
-    const first = jest.fn();
-    const second = jest.fn();
+    const firstCallback = jest.fn();
+    const secondCallback = jest.fn();
 
     Math.random = jest.fn().mockReturnValue(0.4);
 
-    ifElse(condition, first, second);
+    ifElse(condition, firstCallback, secondCallback);
 
-    expect(first).not.toHaveBeenCalled();
-    expect(second).toHaveBeenCalled();
+    expect(firstCallback).not.toHaveBeenCalled();
+    expect(secondCallback).toHaveBeenCalled();
   });
 
   it('call first function when M.random returns value greater than -1', () => {
     const condition = () => Math.random() > -1;
-    const first = jest.fn();
-    const second = jest.fn();
+    const firstCallback = jest.fn();
+    const secondCallback = jest.fn();
 
     const originalMathRandom = Math.random;
 
     Math.random = jest.fn().mockReturnValue(0);
 
-    ifElse(condition, first, second);
-    expect(first).toHaveBeenCalled();
-    expect(second).not.toHaveBeenCalled();
+    ifElse(condition, firstCallback, secondCallback);
+    expect(firstCallback).toHaveBeenCalled();
+    expect(secondCallback).not.toHaveBeenCalled();
     Math.random = originalMathRandom;
   });
 });
