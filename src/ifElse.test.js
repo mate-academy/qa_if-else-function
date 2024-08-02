111851409d8ed897100f0c7c54556f27c15236c5
Kate Hubko <gubkoekaterina9592@gmail.com>
2024-08-02 10:40:24 +0300

    qweqwe

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,31 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const ifElse = require('./ifElse'); // Правильний імпорт функції
 
-  it('should ', () => {
-
+  it('should return undefined', () => {
+    expect(ifElse(() => {}, () => {}, () => {})).toBeUndefined();
   });
 
-  // write tests here
+  it('should call the first callback if condition returns true', () => {
+    const firstFn = jest.fn();
+    const secondFn = jest.fn();
+    const conditionFn = jest.fn(() => true);
+
+    ifElse(conditionFn, firstFn, secondFn);
+
+    expect(firstFn).toHaveBeenCalledTimes(1);
+    expect(secondFn).not.toHaveBeenCalled();
+  });
+
+  it('should call the second callback if condition returns false', () => {
+    const firstFn = jest.fn();
+    const secondFn = jest.fn();
+    const conditionFn = jest.fn(() => false);
+
+    ifElse(conditionFn, firstFn, secondFn);
+
+    expect(firstFn).not.toHaveBeenCalled();
+    expect(secondFn).toHaveBeenCalledTimes(1);
+  });
 });
