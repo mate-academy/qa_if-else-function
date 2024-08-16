b355d8ae8edcd33895cdb7bad0c6fd8aaf91f507
Olaf <olaf.chuszno@gmail.com>
2024-08-16 14:04:19 +0200

    Write tests for ifElse function

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,10 +1,59 @@
 'use strict';
 
-describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+describe('ifElse function', () => {
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+  it('should not pass arguments to the callback', () => {
+    const fn = jest.fn();
 
+    ifElse(() => true, fn, () => { });
+
+    expect(fn)
+      .toHaveBeenCalledWith();
+  });
+
+  it('should not return a value', () => {
+    const result = ifElse(() => true, () => 'test', () => 'test');
+
+    expect(result).toBeUndefined();
+  });
+
+  it('should call first callback if condition passes', () => {
+    const fn = jest.fn();
+
+    ifElse(() => true, fn, () => { });
+
+    expect(fn)
+      .toHaveBeenCalled();
+  });
+
+  it('should not call second callback if condition passes', () => {
+    const fn = jest.fn();
+
+    ifElse(() => true, () => { }, fn);
+
+    expect(fn)
+      .not
+      .toHaveBeenCalled();
+  });
+
+  it(`should call second callback if condition doesn't pass`, () => {
+    const fn = jest.fn();
+
+    ifElse(() => false, () => { }, fn);
+
+    expect(fn)
+      .toHaveBeenCalled();
+  });
+
+  it(`should not call first callback if doesn't pass`, () => {
+    const fn = jest.fn();
+
+    ifElse(() => false, fn, () => { });
+
+    expect(fn)
+      .not
+      .toHaveBeenCalled();
   });
 
   // write tests here
