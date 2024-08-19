e798dbf3616747d46962af9cdd240d482664ca09
boytrend0108 <boytrend@gmail.com>
2024-08-19 06:40:10 +0300

    complete

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,64 @@
-'use strict';
+/* eslint-disable quotes */
+"use strict";
 
-describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+describe("ifElse", () => {
+  const { ifElse } = require("./ifElse");
 
-  it('should ', () => {
+  it("should run first cb if condition === `true`", () => {
+    const f1 = jest.fn();
 
+    ifElse(
+      () => true,
+      f1,
+      () => false
+    );
+
+    expect(f1).toBeCalled();
   });
 
-  // write tests here
+  it("should run second cb if condition === `false`", () => {
+    const f2 = jest.fn();
+
+    ifElse(
+      () => false,
+      () => {},
+      f2
+    );
+
+    expect(f2).toBeCalled();
+  });
+
+  it("should call first cb without args", () => {
+    const f1 = jest.fn();
+
+    ifElse(
+      () => true,
+      f1,
+      () => false
+    );
+
+    expect(f1).toHaveBeenCalledWith();
+  });
+
+  it("should call second cb without args", () => {
+    const f2 = jest.fn();
+
+    ifElse(
+      () => false,
+      () => [],
+      f2
+    );
+
+    expect(f2).toHaveBeenCalledWith();
+  });
+
+  it("should return `undefined`", () => {
+    const result = ifElse(
+      () => false,
+      () => {},
+      () => {}
+    );
+
+    expect(result).toBeUndefined();
+  });
 });
