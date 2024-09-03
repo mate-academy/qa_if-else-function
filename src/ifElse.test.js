622aa7667a8bc19beb2cf122f8d4a6ca699c3793
Denys209818 <deniskravcuk63@gmail.com>
2024-09-03 23:20:54 +0300

    Load solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,27 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+  it('should call the first func if condition is true', () => {
+    const condition = jest.fn(() => true);
+    const first = jest.fn();
+    const second = jest.fn();
 
+    ifElse(condition, first, second);
+
+    expect(second).toHaveBeenCalledTimes(0);
+    expect(first).toHaveBeenCalledTimes(1);
   });
 
-  // write tests here
+  it('should call the second func if condition is false', () => {
+    const condition = jest.fn(() => false);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(first).toHaveBeenCalledTimes(0);
+    expect(second).toHaveBeenCalledTimes(1);
+  });
 });
