32ea805814e3cb35acfe4c74e05de9550d8bda9a
Yevhen Baiev <baev838@gmail.com>
2024-09-15 20:25:12 +0300

    Tests for ifElse function are prepeared

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,44 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+  let conditionTrue;
+  let conditionFalse;
+  let first;
+  let second;
 
+  beforeEach(() => {
+    conditionTrue = jest.fn(() => true);
+    conditionFalse = jest.fn(() => false);
+    first = jest.fn();
+    second = jest.fn();
   });
 
-  // write tests here
+  afterEach(() => {
+    conditionTrue.mockClear();
+    conditionFalse.mockClear();
+    first.mockClear();
+    second.mockClear();
+  });
+
+  it(`shouldn't return nothing`, () => {
+    expect(ifElse(conditionTrue, first, second)).toBeUndefined();
+  });
+
+  it('should call first if condistion returns true', () => {
+    ifElse(conditionTrue, first, second);
+
+    expect(conditionTrue).toHaveBeenCalledTimes(1);
+    expect(first).toHaveBeenCalledTimes(1);
+    expect(second).not.toHaveBeenCalled();
+  });
+
+  it('should call second if condition returns false', () => {
+    ifElse(conditionFalse, first, second);
+
+    expect(conditionFalse).toHaveBeenCalledTimes(1);
+    expect(first).not.toHaveBeenCalled();
+    expect(second).toHaveBeenCalledTimes(1);
+  });
 });
