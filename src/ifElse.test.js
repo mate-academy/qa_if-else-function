dfc1e03046d8166513e6fed93c83c347a35ef994
kaplunova <melamori1605@gmail.com>
2024-10-24 14:53:40 +0300

    added tests for ifElse func

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,46 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
-
+  it('should be declared', () => {
+    expect(ifElse).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it('should call condition cb', () => {
+    const cond = jest.fn(() => {});
+    const cb1 = jest.fn(() => {});
+    const cb2 = jest.fn(() => {});
+
+    ifElse(cond, cb1, cb2);
+    expect(cond).toHaveBeenCalled();
+  });
+
+  it('should run first cb if condition return true', () => {
+    const cond = jest.fn(() => true);
+    const cb1 = jest.fn(() => {});
+    const cb2 = jest.fn(() => {});
+
+    ifElse(cond, cb1, cb2);
+    expect(cb1).toHaveBeenCalled();
+  });
+
+  it('should run second cb if condition return false', () => {
+    const cond = jest.fn(() => false);
+    const cb1 = jest.fn(() => {});
+    const cb2 = jest.fn(() => {});
+
+    ifElse(cond, cb1, cb2);
+    expect(cb2).toHaveBeenCalled();
+  });
+
+  it('should return undefined', () => {
+    const cond = jest.fn(() => false);
+    const cb1 = jest.fn(() => {});
+    const cb2 = jest.fn(() => {});
+
+    const result = ifElse(cond, cb1, cb2);
+
+    expect(result).toBe(undefined);
+  });
 });
