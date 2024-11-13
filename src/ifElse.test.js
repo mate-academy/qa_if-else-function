f3b3a118d5fb5c7d1dbaa1e7bb6905904cf7188b
Ihor Korshylivskyi <ihorkorshylivskyi@gmail.com>
2024-11-13 11:58:56 +0200

    Solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,48 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
+  let mockCondition;
+  let mockFirst;
+  let mockSecond;
 
-  it('should ', () => {
-
+  beforeEach(() => {
+    mockCondition = jest.fn();
+    mockFirst = jest.fn();
+    mockSecond = jest.fn();
   });
 
-  // write tests here
+  it(`should be declared`, () => {
+    expect(ifElse).toBeInstanceOf(Function);
+  });
+
+  it(`shouldn't return anything`, () => {
+    const result = ifElse(mockCondition, mockFirst, mockSecond);
+
+    expect(result).toBeUndefined();
+  });
+
+  it(`should invoke 'condition' callback`, () => {
+    ifElse(mockCondition, mockFirst, mockSecond);
+
+    expect(mockCondition).toHaveBeenCalled();
+  });
+
+  it(`should invoke the 'first' callback if`
+  + ` 'condition' is true`, () => {
+    mockCondition = jest.fn(() => 2 > 1);
+
+    ifElse(mockCondition, mockFirst, mockSecond);
+
+    expect(mockFirst).toHaveBeenCalled();
+  });
+
+  it(`should invoke the 'second' callback if`
+  + ` 'condition' is false`, () => {
+    mockCondition = jest.fn().mockReturnValue(false);
+
+    ifElse(mockCondition, mockFirst, mockSecond);
+
+    expect(mockSecond).toHaveBeenCalled();
+  });
 });
