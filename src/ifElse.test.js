af9a76905dd9eacdfb750599244c244221bf7e88
OleksiiMishchenko <alex.mishch@gmail.com>
2024-11-25 18:26:26 +0200

    Solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,59 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+  it('should be declared', () => expect(ifElse).toBeDefined());
 
+  it('should not return value', () => {
+    const condition = jest.fn();
+    const first = jest.fn();
+    const second = jest.fn();
+
+    const result = ifElse(condition, first, second);
+
+    expect(result).toBeUndefined();
   });
 
-  // write tests here
+  it(`should run 'condition' once`, () => {
+    const condition = jest.fn().mockReturnValue(true);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(condition).toHaveBeenCalledTimes(1);
+  });
+
+  it(`should run 'first' if 'condition' is 'true'`, () => {
+    const condition = jest.fn().mockReturnValue(true);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(first).toHaveBeenCalledTimes(1);
+    expect(second).not.toHaveBeenCalled();
+  });
+
+  it(`should run 'second' if 'condition' is 'false'`, () => {
+    const condition = jest.fn().mockReturnValue(false);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(second).toHaveBeenCalledTimes(1);
+    expect(first).not.toHaveBeenCalled();
+  });
+
+  it(`should call 'condition' without arguments`, () => {
+    const condition = jest.fn().mockReturnValue(true);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(condition).toHaveBeenCalledWith();
+  });
 });
