6aefe435cecbeb92c0dcd5b5cd6936df568fb28c
Galers <galosaandrew@gmail.com>
2024-12-10 18:16:26 +0200

    test: Add test

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,50 @@
-'use strict';
+"use strict";
 
-describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+describe("ifElse", () => {
+  const { ifElse } = require("./ifElse");
 
-  it('should ', () => {
+  it(`should call 'first' if condition returns 'true'`, () => {
+    const condition = jest.fn(() => true);
+    const first = jest.fn();
+    const second = jest.fn();
 
+    ifElse(condition, first, second);
+
+    expect(condition).toHaveBeenCalledTimes(1);
+    expect(first).toHaveBeenCalledTimes(1);
+    expect(second).toHaveBeenCalledTimes(0);
   });
 
-  // write tests here
+  it(`should call 'second' if condition returns 'false'`, () => {
+    const condition = jest.fn(() => false);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(condition).toHaveBeenCalledTimes(1);
+    expect(first).toHaveBeenCalledTimes(0);
+    expect(second).toHaveBeenCalledTimes(1);
+  });
+
+  it(`should call 'condition' without arguments`, () => {
+    const condition = jest.fn(() => true);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(condition).toHaveBeenCalledWith();
+  });
+
+  it(`should handle null return from condition`, () => {
+    const condition = jest.fn(() => null);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(first).not.toHaveBeenCalled();
+    expect(second).toHaveBeenCalledTimes(1);
+  });
 });
