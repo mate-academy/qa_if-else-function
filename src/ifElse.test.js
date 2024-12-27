d08a7a888f8e08d3234143d703ce88719f8a1b4f
Dmytro Boiko <iamboikodmytro@gmail.com>
2024-12-27 11:00:30 +0200

    the task is complete

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,31 @@
-'use strict';
+"use strict";
 
-describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+describe("ifElse", () => {
+  const { ifElse } = require("./ifElse");
 
-  it('should ', () => {
+  it(`should call 'first' func if 'condition' return 'true'`, () => {
+    const condition = jest.fn(() => true);
 
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(condition).toHaveBeenCalled();
+    expect(first).toHaveBeenCalled();
+    expect(second).not.toHaveBeenCalled();
   });
 
-  // write tests here
+  it(`should call 'second' func if 'condition' return 'false'`, () => {
+    const condition = jest.fn(() => false);
+
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(condition).toHaveBeenCalled();
+    expect(first).not.toHaveBeenCalled();
+    expect(second).toHaveBeenCalled();
+  });
 });
