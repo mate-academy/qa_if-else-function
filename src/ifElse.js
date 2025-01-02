6fb72b7eca562a5b37d1b0c173a54f5a16047860
Diana Khalaim <khalaimdiana@gmail.com>
2025-01-02 10:22:25 +0200

    Solution

--- src/ifElse.js
+++ src/ifElse.js
@@ -6,6 +6,12 @@
  * @param second
  */
 function ifElse(condition, first, second) {
+  if (typeof condition !== 'function'
+      || typeof first !== 'function'
+      || typeof second !== 'function') {
+    throw new Error('All arguments must be functions');
+  }
+
   if (condition() === true) {
     first();
   } else {
