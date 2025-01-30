09e52ad42ccc5bd109dc2def9f90e2de772b3ba1
Svitlana <svitlana.gaiduk@gmail.com>
2025-01-30 15:45:49 +0200

    add task solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,43 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
-
+  it('should be an instance of function', () => {
+    expect(ifElse).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it('should call condition only once', () => {
+    const condition = jest.fn(() => true);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(condition).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call first callback when condition returns true', () => {
+    const condition = jest.fn(() => true);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(condition).toHaveBeenCalled();
+    expect(first).toHaveBeenCalled();
+    expect(second).not.toHaveBeenCalled();
+  });
+
+  it('should call second callback when condition returns false', () => {
+    const condition = jest.fn(() => false);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(condition).toHaveBeenCalled();
+    expect(first).not.toHaveBeenCalled();
+    expect(second).toHaveBeenCalled();
+  });
 });
