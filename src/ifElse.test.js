6f42a0e9c4943793dcb6156efaaf76518e57609e
Oleksandr Kovalchuk <kov280396@gmail.com>
2025-03-18 10:14:50 +0200

    Wrote tests for ifElse function

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,61 @@
+/* eslint-disable no-console */
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+  let consoleSpy;
 
+  beforeEach(() => {
+    consoleSpy = jest.spyOn(console, 'log').mockImplementation();
   });
 
-  // write tests here
+  afterEach(() => {
+    consoleSpy.mockRestore();
+  });
+
+  it('should call first callback when condition returns true', () => {
+    const condition = jest.fn().mockReturnValue(true);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(condition).toHaveBeenCalledTimes(1);
+    expect(condition).toHaveBeenCalledWith();
+    expect(first).toHaveBeenCalledTimes(1);
+    expect(first).toHaveBeenCalledWith();
+    expect(second).not.toHaveBeenCalled();
+  });
+
+  it('should call second callback when condition returns false', () => {
+    const condition = jest.fn().mockReturnValue(false);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(condition).toHaveBeenCalledTimes(1);
+    expect(condition).toHaveBeenCalledWith();
+    expect(first).not.toHaveBeenCalled();
+    expect(second).toHaveBeenCalledTimes(1);
+    expect(second).toHaveBeenCalledWith();
+  });
+
+  it('should log correct value based on condition result', () => {
+    ifElse(
+      () => true,
+      () => console.log(1),
+      () => console.log(2)
+    );
+    expect(consoleSpy).toHaveBeenCalledWith(1);
+    consoleSpy.mockClear();
+
+    ifElse(
+      () => false,
+      () => console.log(1),
+      () => console.log(2)
+    );
+    expect(consoleSpy).toHaveBeenCalledWith(2);
+  });
 });
